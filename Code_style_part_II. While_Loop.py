208b45f0bdde211838af64bb250c4303d2c3c1b4
Антон <katrinnnn.tgr@gmail.com>
2024-11-30 15:37:31 +0300

    Update Code_style_part_II. While_Loop.py

--- Code_style_part_II. While_Loop.py
+++ Code_style_part_II. While_Loop.py
@@ -1,6 +1,8 @@
 my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
 positive_numbers = []
 for i in range(len(my_list)):
+    if my_list[i] == 0:
+        break
     if my_list[i] < 0:
         break
     if my_list[i] > 0:
